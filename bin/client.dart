ce130958e5cab85f6c0b19a39903e068814071ae
s.masoumbeigi <sbeygi712@gmail.com>
2024-10-05 08:03:08 +0330

    feat: created a dio client and pretty dio logger

--- bin/client.dart
+++ bin/client.dart
@@ -1,10 +1,34 @@
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'pretty_dio_logger.dart';
+
 const baseUrl = 'http://172.16.251.76:7003/ords/cif';
-class HttpClient{
-  static final HttpClient _client = HttpClient._internal();
-  factory HttpClient() {
-    return _client;
+
+class NetworkModule {
+  static final NetworkModule _instance = NetworkModule._internal();
+
+  NetworkModule._internal();
+
+  factory NetworkModule() {
+    return _instance;
   }
 
-  HttpClient._internal();
+  Dio get dio => _getDio();
+
+  PrettyDioLogger get prettyDioLogger => PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: true,
+      error: true,
+      compact: true,
+      maxWidth: 120);
+
+  _getDio() {
+    var dio = Dio(BaseOptions(baseUrl: baseUrl));
+    dio.interceptors.add(prettyDioLogger);
+    dio.options.connectTimeout = const Duration(milliseconds: 60000);
+    dio.options.receiveTimeout = const Duration(milliseconds: 60000);
+    dio.options.sendTimeout = const Duration(milliseconds: 60000);
+    return dio;
+  }
 }
