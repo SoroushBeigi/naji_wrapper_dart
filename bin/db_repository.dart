188953993eb471dce7358f81f46f49b0a8ea385d
s.masoumbeigi <sbeygi712@gmail.com>
2024-10-07 16:51:05 +0330

    feat(db): created abstract interface for database repository

--- bin/db_repository.dart
+++ bin/db_repository.dart
@@ -1,7 +1,41 @@
-class DatabaseRepository{
-  static final DatabaseRepository instance = DatabaseRepository._internal();
+import 'models/service_model.dart';
 
-  DatabaseRepository._internal();
+abstract interface class DatabaseRepository<T>{
+  Future<void> write(T model);
+  Future<T?> getById(String id);
+  Future<List<T>> getAll();
+  Future<void> update(String id, T model);
+  Future<void> delete(String id);
+}
 
-  Future<void>
+class ServiceRepository implements DatabaseRepository<ServiceModel>{
+  @override
+  Future<void> delete(String id) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ServiceModel>> getAll() {
+    // TODO: implement getAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ServiceModel?> getById(String id) {
+    // TODO: implement getById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> update(String id, ServiceModel model) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> write(ServiceModel model) {
+    // TODO: implement write
+    throw UnimplementedError();
+  }
 }
